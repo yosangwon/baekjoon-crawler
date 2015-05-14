af01efaa9e73a76276fdeb3da893f6e5a717c268
Leo Sangwon Yi <public@leo.re.kr>
2015-05-14 10:04:00 +0900

    폴더 컨트롤러/뷰 수정   - 폴더 컨트롤러에서 404 에러를 던질 수 있도록   - Folder 모델에서 slug를 찾을때 맨 마지막에 슬래시를 넣도록 수정.   - 폴더 뷰에서 바로 baekjoon online judge로 바로 이동할 수 있는 링크 제공.

--- app/controllers/folder_controller.rb
+++ app/controllers/folder_controller.rb
@@ -1,9 +1,11 @@
 class FolderController < ApplicationController
   def index
     @folder = if params[:folder]
-                Folder.find_by(slug: params[:folder])
+                Folder.find_by(slug: "#{params[:folder]}/")
               else
                 Folder.first
               end
+
+    not_found unless @folder
   end
 end
